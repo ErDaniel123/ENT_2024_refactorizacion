2d4bed90d1d06b87aa941b65f8fd220d5da8385a
Mañana <Maana@PC-17>
2024-04-16 09:57:30 +0200

    Solucion final ej2

--- src/ejercicio2/Cliente.java
+++ src/ejercicio2/Cliente.java
@@ -2,4 +2,5 @@ package ejercicio2;
 
 public class Cliente {
 
+	// atributos y metodos de cliente
 }
--- src/ejercicio2/Pedido.java
+++ src/ejercicio2/Pedido.java
@@ -1,5 +1,5 @@
 package ejercicio2;
 
 public class Pedido {
-
+	// atributos y metodos de pedido
 }
