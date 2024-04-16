74aa15ee8e32f99abcdd195ba87bcc626831b988
Mañana <Maana@PC-17>
2024-04-16 09:56:52 +0200

    Solucion ejercicio 2

--- src/ejercicio2/MetodosTienda.java
+++ src/ejercicio2/MetodosTienda.java
@@ -2,4 +2,10 @@ package ejercicio2;
 
 public class MetodosTienda {
 
+	 public double calcularPrecioTotalPedido(Pedido pedido) {
+			return 0;
+	        // Lógica para calcular el precio total de un pedido
+	    }
+
+	    // Otros métodos relacionados con la gestión de la tienda
 }
--- src/ejercicio2/Start.java
+++ src/ejercicio2/Start.java
@@ -3,7 +3,9 @@ package ejercicio2;
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*Crea a los clientes , pedidos y productos y realiza las operaciones 
+		y metodos necesarios para mostrar la informacion que se indique*/
 
 	}
 
